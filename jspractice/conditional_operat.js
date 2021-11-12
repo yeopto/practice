3c8f088b909bd546e81cb71db10511a821cb90e2
김건엽 <yeopto@YeopBookAir.local>
2021-11-12 20:56:15 +0900

    2021-11-12

---
+++ jspractice/conditional_operat.js
@@ -0,0 +1,11 @@
+//조건식 ? 참 일때 실행되는 식 : 거짓 일때 실행되는 식
+let condition = true;
+let value = condition ? '참' : '거짓';
+console.log(value);
+
+if (condition) {
+    value = '참';
+} else {
+    value = '거짓';
+}
+console.log(value);
